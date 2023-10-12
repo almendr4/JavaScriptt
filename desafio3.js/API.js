bf309d4f282cb7c36e0c480a764b58fa22b4c013
almendr4 <134632140+almendr4@users.noreply.github.com>
2023-10-12 02:39:18 -0300

    Update API.js

--- desafio3.js/API.js
+++ desafio3.js/API.js
@@ -1,1 +1,45 @@
+async function obtenerUsuarios(){
+  const respuesta = await fetch("https://dummyjson.com/users")
+  const usuarios = await respuesta.json()
+  return usuarios.users
+}
 
+
+async function listarNombresHombre(){
+  const data = await obtenerUsuarios()
+  const personas = data.map((persona) => {
+    return{
+      nombre: persona.name,
+      edad: persona.age,
+      pais: persona.country,
+    };
+  });
+  
+  let totalEdades = 0;
+  let personasMayores18 = 0;
+  let pesonasMenores35 = 0;
+  
+  personas.forEach((persona) =>{
+    totalEdades += persona.edad;
+    if (persona.edad > 18) {
+      personasMayores18++;
+    }
+    if (persona.edad < 35) {
+      personasMenores35++;
+    }
+  });
+  const promedioEdades = totalEdades / personas.length;
+  const porcentajeMayores18 = (personasMayores18 / personas.length) * 100;
+  const porcentajeMenores35 = (personasMenores35 / personas.length) * 100;
+  
+  
+  console.log('- - - - Informacion acerca de las 100 personas - - - -');
+  console.log('Promedio de Edades:', promedioEdades.toFixed(2));
+  console.log('Porcentaje de personas Mayores de 18:', porcentajeMayores18.toFixed(2) + '%');
+  console.log('Porcentaje de personas Menores de 35:', porcentajeMenores35.toFixed(2) + '%');
+  console.log('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -');
+  
+}
+
+
+listarNombresHombre()
